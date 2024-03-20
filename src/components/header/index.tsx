a114721f67bdf9951dff0c3c3531207795c1cf35
danilojssantos <danilojssantos1@gmail.com>
2024-03-20 19:50:14 -0300

    iniciando o header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,27 @@
+import logoImg from '../../assets/logo.svg'
+import {Link} from 'react-router-dom'
+import {FiUser} from 'react-icons/fi'
+
 export function Header(){
+const signed = false;
+const loadingAuth = false;
+
+
   return(
-    <div>
-          <h1> Componente Header </h1>
+    <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
+          <header className="flex e-full max-w-7x1 items-center justify-between px-4 mx-auto">
+            <Link to="/"> 
+                <img src={logoImg} 
+                alt="logo do site" 
+                
+                />
+            
+            </Link>
+
+            <Link to='/dashboard'>
+              <FiUser size={24} color='#000' />          
+            </Link>
+          </header>
     </div>
   )
 }
