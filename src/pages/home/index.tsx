996c6ee03277a7ba2d5f3dea6d15b6d523c9278f
danilojssantos <danilojssantos1@gmail.com>
2024-03-21 20:59:56 -0300

    section com input e button na home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,15 @@ import { Container } from "../../components/container";
 export function Home(){
   return(
     <Container>
-          <h1> Pagina inicial </h1>
+      <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex
+      justify-center gap-2">
+        <input className="w-full border-2 rounded-lg h-9 px-3 outline-none"
+          placeholder="Digite o nome do carro"
+        />
+        <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">
+          Buscar
+        </button>
+      </section>
     </Container>
   )
 }
