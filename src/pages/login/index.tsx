899b84356944ee225e983bf163b46bd34566ccd2
danilojssantos <danilojssantos1@gmail.com>
2024-03-22 21:19:17 -0300

    vslidação das input email e senha da  tela de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,61 @@
 import logoImg from "../../assets/logo.svg"
 import { Link } from "react-router-dom"
 import { Container } from "../../components/container"
+import { Input } from '../../components/input'
+import {useForm} from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const schema = z.object({
+  email: z.string().email("Insira um email válido").min(1, "campo de email é obrigatório"),
+  password: z.string().min(1, "o campo de email é obrigatório"),
+})
+
+type FormData = z.infer<typeof schema>
 export function Login(){
+  const {register,handleSubmit,formState:{errors} } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode:"onChange"
+  })
+
+  function onSubmit(data:FormData){
+    console.log(data)
+  }
   return(
     <Container>
-    <div className="w-full min-h-screen flex justify-center 
-    items-center flex-col gap-4">
+    
+    <div className="w-full min-h-screen flex justify-center items-center 
+    flex-col gap-4">
       <Link to="/" className="mb-6 max-w-sm w-full">
         <img src={logoImg} alt="Logo do site"
         className="w-full" />
       </Link>
-         
+         <form 
+         className="bg-white max-w-xl w-full rounded-lg"
+         onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3">
+            <Input 
+            type="email" placeholder="digite seu email"
+            name="email"
+            error={errors.email?.message}
+            register={register}
+            />
+
+          </div>  
+          <div className="mb-3">
+            <Input 
+            type="password" placeholder="digite sua senha"
+            name="password"
+            error={errors.password?.message}
+            register={register}
+            />
+
+          </div>  
+          <button>
+            Acessar
+          </button>
+
+         </form>
     </div>
     </Container>
   )
